7138dbbf6a08732b3a02ea7337402de4f65ce37e
Vaughan Webb <vaughanw2012@gmail.com>
2020-03-05 14:36:58 +1300

    cam

--- Assets/Scripts/print.cs
+++ Assets/Scripts/print.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class print : MonoBehaviour
 {
     public RenderTexture renderTexture;
+    public GameObject test;
+
+    protected private Texture2D tex2d;
 
     public void tackpic()
     {
-        Texture2D tex2d = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+        tex2d = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
         RenderTexture.active = renderTexture;
         tex2d.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
         tex2d.Apply();
+        test.GetComponent<MeshRenderer>().material.SetTexture("Texture2D_2BAEFA0B", tex2d);
     }
 }
