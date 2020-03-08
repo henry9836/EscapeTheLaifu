4c465d48985c9c272646cb8041138d32e3faf83f
Vaughan Webb <vaughanw2012@gmail.com>
2020-03-09 09:40:49 +1300

    getitng pause set up[

---
+++ Assets/Scripts/VR/pausebutton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class pausebutton : MonoBehaviour
+{
+    public bool isPaused = false;
+    public SteamVR_Action_Boolean inputAction = null;
+    private SteamVR_Behaviour_Pose tmpPose = null;
+    public bool once = true;
+
+    private GameObject leftControler;
+    private GameObject rightControler;
+
+    void Awake()
+    {
+        tmpPose = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+
+    void Start()
+    {
+        leftControler = GameObject.Find("[CameraRig]").transform.GetChild(0).gameObject;
+        rightControler = GameObject.Find("[CameraRig]").transform.GetChild(1).gameObject;
+
+    }
+
+
+    void Update()
+    {
+        if (inputAction.GetStateDown(tmpPose.inputSource))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused == true)
+        {
+            if (once == true)
+            {
+                once = false;
+                leftControler.GetComponent<Pointer>().enabled = true;
+                rightControler.GetComponent<Pointer>().enabled = true;
+
+                //enable UI 
+            }
+        }
+        else 
+        {
+            if (once == false)
+            {
+                once = true;
+                leftControler.GetComponent<Pointer>().enabled = false;
+                rightControler.GetComponent<Pointer>().enabled = false;
+                //disable UI
+            }
+        }
+    }
+
+
+}
