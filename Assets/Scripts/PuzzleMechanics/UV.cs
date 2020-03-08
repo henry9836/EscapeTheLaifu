dc867ee943cf54bc747944202c2d8f57441be96e
henry9836 <henry983615@gmail.com>
2020-03-08 14:14:21 +1300

    at least it's starting to work now

--- Assets/Scripts/PuzzleMechanics/UV.cs
+++ Assets/Scripts/PuzzleMechanics/UV.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Light))]
 public class UV : MonoBehaviour
 {
     
@@ -9,161 +10,78 @@ public class UV : MonoBehaviour
     {
         public void refresh(Vector3 forwardLightDir, Vector3 lightPosition)
         {
-
+            //Get Beam Info
 
             float angleOfBeam = light.spotAngle * 0.5f;
             float distanceOfBeam = light.range;
 
+            beamDir = Quaternion.AngleAxis(angleOfBeam, Vector3.forward) * forwardLightDir;
+            beamPos = lightPosition + (beamDir * distanceOfBeam);
 
-            //Get Torch Beam Directions From Angle
+            centerBeamPos = lightPosition + (forwardLightDir * distanceOfBeam);
 
-            /*
-            var q = Quaternion.AngleAxis(angle, Vector3.forward);
-            newPosition = currentPosition + q * Vector3.right * distance;
+            //Build Lists
+
+            float amountofSphereChecks = distanceOfBeam / stepDistance;
+
+            float distanceFromOrigin = 0.0f;
+            float radius = 0.0f;
+            Vector3 radiusPos = Vector3.zero;
+            Vector3 lightBeamPos = Vector3.zero;
             
-            ⟨→BA⟩=(2,3)−(−3,2)=⟨5,1⟩
-            */
 
-
-            //Debug.Log($"Angle [{Mathf.Deg2Rad * Vector3.Angle((uvObjects[0].transform.position - lightPosition), forwardLightDir)}/{Mathf.Deg2Rad * angleOfBeam}]");
-            //Debug.DrawLine(lightPosition, uvObjects[0].transform.position, Color.red);
-            tLDir = Quaternion.AngleAxis(angleOfBeam, Vector3.forward) * forwardLightDir;
-            tRDir = Quaternion.AngleAxis(-angleOfBeam, Vector3.forward) * forwardLightDir;
-            //Vector3 tRDir1 = Quaternion.AngleAxis(angleOfBeam, Vector3.forward) * forwardLightDir;
-            //Vector3 tRDir2 = Quaternion.AngleAxis(angleOfBeam, Vector3.up) * forwardLightDir;
-            //tRDir = tRDir1 + tRDir2;
-
-            //Vector3 bLDir1 = Quaternion.AngleAxis(-angleOfBeam, Vector3.forward) * forwardLightDir;
-            //Vector3 bLDir2 = Quaternion.AngleAxis(-angleOfBeam, Vector3.up) * forwardLightDir;
-            //bLDir = bLDir1 + bLDir2;
-
-            ////tLDir = tRDir1 + bLDir2;
-            //bRDir = bLDir1 + tRDir2;
-
-            tRPos = lightPosition + (tRDir * distanceOfBeam);
-            tLPos = lightPosition + (tLDir * distanceOfBeam);
-            //bLPos = lightPosition + (bLDir * distanceOfBeam);
-            //bRPos = lightPosition + (bRDir * distanceOfBeam);
-            //Get Torch Positions
-
-
-
-            Vector3 coneBasePos = lightPosition + (forwardLightDir * distanceOfBeam);
-
-            float radius = Vector3.Distance(coneBasePos, tRPos);
-
-            coneNormals.x = (coneBasePos.x - lightPosition.x) / distanceOfBeam;
-            coneNormals.y = (coneBasePos.y - lightPosition.y) / distanceOfBeam;
-            coneNormals.z = (coneBasePos.z - lightPosition.z) / distanceOfBeam;
-
-            float C = radius * radius / distanceOfBeam;
-
-            float Y0 = coneNormals.x * lightPosition.x + coneNormals.y * lightPosition.y + coneNormals.z * lightPosition.z;
-
-            Vector3 target = uvObjects[0].transform.position;
-
-            float coneY = target.x * coneNormals.x + target.y * coneNormals.y + target.z * coneNormals.z - Y0;
-            if (coneY < 0)
+            //For each stepcheck
+            for (int i = 0; i < amountofSphereChecks; i++)
             {
-                //Debug.Log("Outside");
-            }
-            else if (coneY > distanceOfBeam)
-            {
-                //Debug.Log("Outside");
-            }
+                //i * stepdistance = distance of check
+                //Vector3.Distance(lightBeam.beamPos, lightBeam.centerBeamPos) at point is equal to radius
 
-            float tempX = target.x - lightPosition.x - coneY * coneNormals.x;
-            float tempY = target.y - lightPosition.y - coneY * coneNormals.y;
-            float tempZ = target.z - lightPosition.z - coneY * coneNormals.z;
+                //Physics Check
+                distanceFromOrigin = i * stepDistance;
 
-            float coneR2 = tempX * tempX + tempY * tempY + tempZ * tempZ;
+                //Get positions
+                lightBeamPos = lightPosition + (forwardLightDir * distanceFromOrigin);
+                radiusPos = lightPosition + (beamDir * distanceFromOrigin);
+                radius = Vector3.Distance(lightBeamPos, radiusPos);
 
-            if (coneR2 > C * coneR2)
-            {
-                Debug.Log("Outside");
-            }
-            else
-            {
-                Debug.Log("Inside");
+                //Perform a sphere cast
+                Collider[] cols = Physics.OverlapSphere(lightBeamPos, radius);
+
+                //If we hit objects
+                for (int j = 0; j < cols.Length; j++)
+                {
+                    Debug.Log($"I HIT [{cols[j].gameObject.name}] WITH AN I OF [{i}]");
+                    //If the object has a uv object component
+                    if (cols[j].gameObject.GetComponent<UVObject>())
+                    {
+                        //Invoke Glow
+                    }
+                }
             }
 
         }
 
-        public Vector3 tLPos = Vector3.zero;
-        public Vector3 tRPos = Vector3.zero;
-        public Vector3 bLPos = Vector3.zero;
-        public Vector3 bRPos = Vector3.zero;
-
-        public Vector3 tLDir = Vector3.zero;
-        public Vector3 tRDir = Vector3.zero;
-        public Vector3 bLDir = Vector3.zero;
-        public Vector3 bRDir = Vector3.zero;
-
-        public Vector3 coneNormals = Vector3.zero;
+        public Vector3 beamPos = Vector3.zero;
+        public Vector3 beamDir = Vector3.zero;
+        public Vector3 centerBeamPos = Vector3.zero;
 
     };
 
-    static Light light;
-    LightBeam lightBeam = new LightBeam();
-    static List<GameObject> uvObjects = new List<GameObject>();
+    public static float stepDistance = 0.1f;
 
-    /*
-
-        For each plane of the tetrahedron, check if the point is on the same side as the remaining vertex:
-
-        bool SameSide(v1, v2, v3, v4, p)
-        {
-            normal := cross(v2 - v1, v3 - v1)
-            dotV4 := dot(normal, v4 - v1)
-            dotP := dot(normal, p - v1)
-            return Math.Sign(dotV4) == Math.Sign(dotP);
-        }
-
-        And you need to check this for each plane:
-
-        bool PointInTetrahedron(v1, v2, v3, v4, p)
-        {
-            return SameSide(v1, v2, v3, v4, p) &&
-                   SameSide(v2, v3, v4, v1, p) &&
-                   SameSide(v3, v4, v1, v2, p) &&
-                   SameSide(v4, v1, v2, v3, p);               
-        }
-
-     */
-
-            bool CheckIfInside(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4, Vector3 point)
-    {
-        Vector3 normal = Vector3.Cross(v2 - v1, v3 - v1);
-        float dotV4 = Vector3.Dot(normal, v4 - v1);
-        float dotP = Vector3.Dot(normal, point - v1);
-        return Mathf.Sign(dotV4) == Mathf.Sign(dotP);
-    }
-
-    bool CheckIfInsideLight()
-    {
-        return false;
-    }
+    private new static Light light = null;
+    private LightBeam lightBeam = new LightBeam();
 
     private void Start()
     {
         light = GetComponent<Light>();
-
-        UVObject[] tmpObjs = FindObjectsOfType<UVObject>();
-
-        for (int i = 0; i < tmpObjs.Length; i++)
-        {
-            uvObjects.Add(tmpObjs[i].gameObject);
-        }
-
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-
         lightBeam.refresh(transform.forward, transform.position);
-        Debug.DrawLine(transform.position, lightBeam.tLPos, Color.red);
-        Debug.DrawLine(transform.position, lightBeam.tRPos, Color.yellow);
-        Debug.DrawLine(transform.position, lightBeam.bLPos, Color.yellow);
-        Debug.DrawLine(transform.position, lightBeam.bRPos, Color.yellow);
+        Debug.DrawLine(transform.position, lightBeam.beamPos, new Color(0.40f, 0.20f, 0.92f));
+        Debug.DrawLine(transform.position, lightBeam.centerBeamPos, Color.red);
+        Debug.Log($"Distance {Vector3.Distance(lightBeam.beamPos, lightBeam.centerBeamPos)}");
     }
 }
