1d3da91d6b940f0f44d5f5d6ce166690704cff20
Henry Oliver <henry983615@gmail.com>
2020-03-11 18:11:21 +1300

    keypad PC weorkds

--- Assets/Scripts/PuzzleMechanics/figureS/slidedown.cs
+++ Assets/Scripts/PuzzleMechanics/figureS/slidedown.cs
@@ -6,16 +6,18 @@ public class slidedown : MonoBehaviour
 {
     public void henryThisOne()
     {
+        Debug.Log("Henry Called!");
         StartCoroutine(slide());
     }
 
     public IEnumerator slide()
     {
+        Debug.Log("Time to slide ");
         Vector3 startpos = this.gameObject.transform.position;
-        Vector3 endpos = startpos + (new Vector3(0.0f, 0.0f, 1.0f) * -0.01402f);
+        Vector3 endpos = startpos + (new Vector3(0.0f, 1.0f, 0.0f) * -0.3f); //-0.01402f 
         this.GetComponent<BoxCollider>().enabled = false;
 
-        for (float i = 0.0f; i < 1.0f; i += Time.unscaledDeltaTime)
+        for (float i = 0.0f; i < 1.0f; i += Time.unscaledDeltaTime * 0.3f)
         {
             this.gameObject.transform.position = Vector3.Lerp(startpos, endpos, i);
             yield return null;
