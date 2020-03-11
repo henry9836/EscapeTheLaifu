32a179b79fac6c2b30b418ec1121a44d432164c6
Vaughan Webb <vaughanw2012@gmail.com>
2020-03-11 13:52:03 +1300

    door slide

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,11 @@ public class GameManager : MonoBehaviour
     public float maxTime = 120.0f;
     public GameObject timerUI;
     public float fadeouttime = 2.0f;
+    public GameObject door;
 
     public bool playing = true;
+
+
     void Start()
     {
         timer = maxTime;
@@ -61,6 +64,7 @@ public class GameManager : MonoBehaviour
         playing = false;
         StartCoroutine(textflash());
         //open door
+        StartCoroutine(doorslide());
         Debug.Log("game over");
     }
 
@@ -92,5 +96,19 @@ public class GameManager : MonoBehaviour
         }
         Debug.Log("load scene");
         SceneManager.LoadScene(0);
+    }
+
+    public IEnumerator doorslide()
+    {
+        Vector3 startpos = door.transform.position;
+        Vector3 endpos = startpos + new Vector3(0.0f, 0.0f, -0.5f);
+        for (float i = 0.0f; i < 1.0f; i += Time.unscaledDeltaTime * 0.3f)
+        {
+            door.transform.position = Vector3.Lerp(startpos, endpos, i);
+            yield return null;
+        }
+
+        yield return null;
+    
     }
 }
